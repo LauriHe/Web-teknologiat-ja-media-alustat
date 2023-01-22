ccec09e6d53adb92e0703e22c862c19ed5411fb3
LauriHe <Lauri.Heikkinen5@metropolia.fi>
2023-01-22 20:35:47 +0200

    week2-task2

--- src/main.js
+++ src/main.js
@@ -18,13 +18,20 @@ const p1 = document.querySelector("#p1");
 const p2 = document.querySelector("#p2");
 const langButton = document.querySelector("#langButton");
 const listsortButton = document.querySelector("#listsortButton");
-const dropdownContent = document.querySelector(".dropdownContent");
-const dropdownButtons = Array.from(dropdownContent.children);
+const sortContent = document.querySelector(".sortContent");
+const filterContent = document.querySelector(".filterContent");
+const sortButtons = Array.from(sortContent.children);
+const filterButtons = Array.from(filterContent.children);
 const mainCards = document.querySelector("#mainCards");
 const dropbtn = document.querySelector(".dropbtn");
-const ascButton = document.querySelector("#asc");
-const descButton = document.querySelector("#desc");
+const nameAscButton = document.querySelector("#nameAsc");
+const namedescButton = document.querySelector("#nameDesc");
+const priceAscButton = document.querySelector("#priceAsc");
+const priceDescButton = document.querySelector("#priceDesc");
+const filterPriceButton = document.querySelector("#filterPrice");
+const filterVegButton = document.querySelector("#filterVeg");
 const randomButton = document.querySelector("#randomButton");
+const menuPrice = document.querySelector("#menuPrice");
 const closeButton = document.querySelector("#closeButton");
 const modal = document.querySelector(".modal");
 const modalContent = document.querySelector(".modalContent");
@@ -32,27 +39,74 @@ const modalContent = document.querySelector(".modalContent");
 let lang = "fi";
 let sort = "";
 
+const menuPriceCalc = () => {
+  menuPrice.textContent +=
+    " " + courses.reduce((a, b) => a + parseFloat(b.price), 0) + " €";
+};
+
+const raisePrices = () => {
+  courses = courses.map((course) => {
+    return {
+      title_fi: course.title_fi,
+      title_en: course.title_en,
+      category: course.category,
+      properties: course.properties,
+      price: (course.price * 1.15).toFixed(2),
+    };
+  });
+  console.log(courses);
+};
+
 const sortCourses = (sort) => {
-  if (sort === "") {
-    return courses;
-  }
-  if (sort === "asc") {
+  if (sort === "nameAsc") {
     const sortedCourses = courses.sort((a, b) => {
       if (eval("a.title_" + lang) < eval("b.title_" + lang)) {
         return -1;
       }
     });
-    sort = "asc";
+    sort = "nameAsc";
     return sortedCourses;
   }
-  if (sort === "desc") {
+  if (sort === "namedesc") {
     const sortedCourses = courses.sort((a, b) => {
       if (eval("a.title_" + lang) > eval("b.title_" + lang)) {
         return -1;
       }
     });
-    sort = "desc";
+    sort = "namedesc";
     return sortedCourses;
+  }
+  if (sort === "priceAsc") {
+    const sortedCourses = courses.sort((a, b) => {
+      if (a.price < b.price) {
+        return -1;
+      }
+    });
+    sort = "priceAsc";
+    return sortedCourses;
+  }
+  if (sort === "priceDesc") {
+    const sortedCourses = courses.sort((a, b) => {
+      if (a.price > b.price) {
+        return -1;
+      }
+    });
+    sort = "priceDesc";
+    return sortedCourses;
+  }
+  return courses;
+};
+
+const filterCourses = (filter) => {
+  if (filter === "price") {
+    const filteredCourses = courses.filter((course) => course.price < 5);
+    return filteredCourses;
+  }
+  if (filter === "veg") {
+    const filteredCourses = courses.filter((course) =>
+      course.properties.includes("Veg")
+    );
+    return filteredCourses;
   }
 };
 
@@ -73,7 +127,7 @@ const renderCards = (courses) => {
     properties.textContent = course.properties;
 
     const price = document.createElement("p");
-    price.textContent = course.price;
+    price.textContent = course.price + " €";
 
     card.appendChild(title);
     card.appendChild(category);
@@ -97,9 +151,15 @@ const changeLang = () => {
   p2.textContent = eval("ui" + lang + ".p2");
   langButton.textContent = eval("ui" + lang + ".langButton");
   dropbtn.textContent = eval("ui" + lang + ".dropbtn");
-  ascButton.textContent = eval("ui" + lang + ".ascButton");
-  descButton.textContent = eval("ui" + lang + ".descButton");
+  nameAscButton.textContent = eval("ui" + lang + ".nameAscButton");
+  namedescButton.textContent = eval("ui" + lang + ".nameDescButton");
+  priceAscButton.textContent = eval("ui" + lang + ".priceAscButton");
+  priceDescButton.textContent = eval("ui" + lang + ".priceDescButton");
+  filterPriceButton.textContent = eval("ui" + lang + ".filterPrice");
+  filterVegButton.textContent = eval("ui" + lang + ".filterVeg");
   randomButton.textContent = eval("ui" + lang + ".randomButton");
+  menuPrice.textContent = eval("ui" + lang + ".menuPrice");
+  menuPriceCalc();
   renderCards(sortCourses(sort));
 };
 
@@ -124,11 +184,20 @@ const renderModal = (course) => {
   modalContent.appendChild(price);
 };
 
-langButton.addEventListener("click", changeLang);
+const validateName = (name) => {
+  const pattern = /^[A-Z].{3,63}/;
+  return pattern.test(name);
+};
 
-dropdownButtons.forEach((child) => {
+sortButtons.forEach((child) => {
   child.addEventListener("click", () => {
     renderCards(sortCourses(child.value));
+  });
+});
+
+filterButtons.forEach((child) => {
+  child.addEventListener("click", () => {
+    renderCards(filterCourses(child.value));
   });
 });
 
@@ -139,8 +208,13 @@ randomButton.addEventListener("click", () => {
 });
 
 closeButton.addEventListener("click", () => {
-  console.log("close");
   modal.classList.add("hidden");
 });
+
+langButton.addEventListener("click", changeLang);
+
+raisePrices();
+
+menuPriceCalc();
 
 renderCards(courses);
--- webpack.common.js
+++ webpack.common.js
@@ -6,7 +6,7 @@ const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   entry: {
-    app: "./src/numbers.js",
+    app: "./src/main.js",
   },
   plugins: [
     new CleanWebpackPlugin(),
@@ -24,7 +24,7 @@ module.exports = {
       meta: {
         viewport: "width=device-width, initial-scale=1.0",
       },
-      template: "./src/numbers.html",
+      template: "./src/main.html",
       minify: {
         removeComments: true,
         collapseWhitespace: true,
